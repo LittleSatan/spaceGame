f7ee6218686560736dfb1b3d2a7aeba363626fa0
Marissa <leow.mannheim@gmail.com>
2017-05-27 02:32:02 +0200

    random world generation
    + added jwagner simplex-noise script thingy
    + added random world generation
    * i still want to die <3

--- content/js/classes/Map.js
+++ content/js/classes/Map.js
@@ -2,10 +2,17 @@ class Map {
     constructor(width, height) {
         this.mapRendered = new Image();
         this.area = new Array(width);
+        let simplex = new SimplexNoise();
         for (let x = 0; x < width; x++) {
             this.area[x] = new Array(height);
             for (let y = 0; y < this.area[x].length; y++) {
-                this.area[x][y] = new Tile(x, y, Math.floor(Math.random() * 42));
+                //                this.area[x][y] = new Tile(x, y, Math.floor(Math.random() * 15 * 6));
+                let value = simplex.noise2D(x * 0.008, y * 0.008);
+                if (value < 0.005) this.area[x][y] = new Tile(x, y, 90);
+                if (value >= 0.005 && value < 0.2) this.area[x][y] = new Tile(x, y, 8);
+                if (value >= 0.2 && value < 0.9) this.area[x][y] = new Tile(x, y, 42);
+                if (value >= 0.9) this.area[x][y] = new Tile(x, y, 48);
+
             }
         }
         for (let x = 0; x < this.area.length; x++) {
